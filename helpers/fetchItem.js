2dfd18cc87c83ef7b245a9eb864a873f129bf8b2
Julia Barcelos <julaai@gmail.com>
2021-10-21 00:47:00 -0300

    correcao do cypress por facha de iniciar uma segunda funcao e ainda nao ter feito a cobertura do codigo

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,9 +1,9 @@
 const fetchItem = (id) => {
-  const url = `https://api.mercadolibre.com/items/${id}`;
-  return fetch(url)
-    .then((response) => response.json())
-    .then((data) => (data))
-    .catch((error) => error);
+  // const url = `https://api.mercadolibre.com/items/${id}`;
+  // return fetch(url)
+  //   .then((response) => response.json())
+  //   .then((data) => (data))
+  //   .catch((error) => error);
 };
 
 if (typeof module !== 'undefined') {
