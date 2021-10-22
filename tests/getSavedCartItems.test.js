e021e31d63a62ede4b6cd378455d13a97caafca9
Julia Barcelos <julaai@gmail.com>
2021-10-21 23:42:34 -0300

    Conclusao dos testes do requisito 4

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -7,6 +7,12 @@ Object.defineProperty(window, 'localStorage', {
 });
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it ('A chamada de getSaveCartItems("<ol><li>Item</li></ol>" retorna o localStorage) ', () => {
+    getSavedCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.getItem).toHaveBeenCalled();
+  });
+  it ('Ao executar a função, o método localStorage.getItem é chamado com o parâmetro `cartItems`', () => {
+    getSavedCartItems('cartItems');
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
+  });
 });
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -7,6 +7,12 @@ Object.defineProperty(window, 'localStorage', {
 });
 
 describe('4 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it ('A chamada de saveCartItems("<ol><li>Item</li></ol>" retorna o localStorage) ', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+  it ('Ao executar a função, o método localStorage.getItem é chamado com o parâmetro `cartItems`', () => {
+    saveCartItems('cartItems');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems','<ol><li>Item</li></ol>' );
+  });
 });
